d399b18b900004d1b6691649aab3efaa6e69ba31
Nachiketh <murthy@manifoldailearning.in>
2024-07-12 07:28:42 +0530

    v2code

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from my CI/CD powered Flask app! V1'
+    return 'Hello from my CI/CD powered Flask app! V2'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    print(f"__name__ is {__name__}")
+    app.run(debug=True, host='0.0.0.0',port=5001)
